378a56b7e0dd8b2c7113dd6ac5d90eb0e42f2451
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-07-07 21:56:09 +0800

    React Fragment

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,36 @@
 import React from 'react'  ;
+import Modal from '../Modal' ;
+import history from '../../history'  ;
 
-const StreamDelete = ()=>{
-    return <div>StreamDelete</div>
-} ;
+
+class StreamDelete  extends React.Component {
+
+    componentDidMount(){
+        console.log(this.props) ;
+    }
+
+ renderActions (){
+   return (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>);
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                StreamDelete
+                <Modal
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this Stream?"
+                    actions={ this.renderActions() }
+                    onDismiss={() => history.push('/')}
+                />
+            </div>);
+    }
+}
 
 export default StreamDelete ;
