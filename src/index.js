f1fbb291b1ba0a99ae7235a7827e4cf26c58085e
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-07-19 23:11:21 +0800

    Delete Streams

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import {createStore , applyMiddleware , compose} from "redux";
 import reduxThunk from 'redux-thunk' ;
 
 
+//const NodeMediaServer = require('node-media-server');
+
 import App from './components/App' ;
+
 import reducers from './reducers' ;
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose ;
